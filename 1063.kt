d1f2046711559a61430f0da56724de0c27d87851
신명철 <82868004+Myeongcheol-shin@users.noreply.github.com>
2023-01-04 22:05:18 +0900

    Update 1063.kt

--- 1063.kt
+++ 1063.kt
@@ -1,4 +1,3 @@
-package BJ_1063
 
 var sx : Int = 0
 var sy : Int = 0
@@ -6,10 +5,10 @@ var cx : Int = 0
 var cy : Int = 0
 fun main() {
     var tmp = readLine()!!.split(" ")
-    var sx = tmp[0][0].toInt() - 64
-    var sy = tmp[0][1].toInt() - 48
-    var cx = tmp[1][0].toInt() - 64
-    var cy = tmp[1][1].toInt() - 48
+    cx = tmp[0][0].toInt() - 64
+    cy = tmp[0][1].toInt() - 48
+    sx = tmp[1][0].toInt() - 64
+    sy = tmp[1][1].toInt() - 48
     var N = tmp[2].toInt()
 
     repeat(N)
@@ -17,33 +16,50 @@ fun main() {
         when(readLine())
         {
             "R" -> {
-
+                move(1,0)
             }
             "L" -> {
-
+                move(-1,0)
             }
             "B" -> {
-
+                move(0,-1)
             }
             "T" -> {
-
+                move(0,1)
             }
             "RT" -> {
-
+                move(1,1)
             }
             "LT" -> {
-
+                move(-1,1)
             }
             "RB" -> {
-
+                move(1,-1)
             }
             "LB" -> {
-
+                move(-1,-1)
             }
         }
     }
+    System.out.printf("%s %s",(64 + cx).toChar() + cy.toString(),(64 + sx).toChar() + sy.toString())
 }
-
+fun move(x : Int, y : Int)
+{
+    if (canMove(x + cx, y + cy)){
+        if (checkStone(x + cx , y + cy)){
+            if(canMove( x + sx, y + sy)){
+                sx += x
+                sy += y
+                cx += x
+                cy += y
+            }
+        }
+        else {
+            cx += x
+            cy += y
+        }
+    }
+}
 fun canMove(x : Int, y : Int) : Boolean
 {
     if (x in 1..8 && y in 1..8) return true
